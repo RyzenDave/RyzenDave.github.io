c5474815cbf4b0fb31f2e745f5b8d20dbbe80022
David.P <davidpejoski15@gmail.com>
2024-12-19 17:12:36 +0100

    New homework added
    hmwrk

---
+++ JAVASCRIPT/Homework6n7/script.js
@@ -0,0 +1,23 @@
+console.log("====== HOMEWORK 6 =======");
+function checkIfPassed(grades, passingAverage) {
+  let total = grades.reduce((sum, grade) => sum + grade, 0);
+  let average = total / grades.length;
+
+  if (average >= passingAverage) {
+    alert("The student has passed the semester!");
+  } else {
+    alert("The student has not passed the semester.");
+  }
+}
+
+let grades = [10, 6, 8, 9, 6];
+let passingAverage = 8;
+checkIfPassed(grades, passingAverage);
+console.log("====== HOMEWORK 7 =======");
+function removeFalsyValues(arr) {
+  return arr.filter(Boolean);
+}
+
+let array = [null, undefined, NaN, "", false, 1, "hello", 42, true, {}];
+let cleanedArray = removeFalsyValues(array);
+console.log(cleanedArray);
